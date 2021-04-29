cb55ce1d7b96321ee3020442912198ae9e54294a
Sergey Rusakov <sergeyrusakov1@yandex.ru>
2021-04-29 23:45:49 +0500

    Refactor AppDialog

--- src/components/app-dialog/AppDialog.tsx
+++ src/components/app-dialog/AppDialog.tsx
@@ -1,16 +1,14 @@
 import React, { ReactNode } from 'react';
 import './AppDialog.css';
 
-interface AppDialogProps<T> {
+interface AppDialogProps {
     isOpen: boolean;
-    afterClose: (returnValue?: T) => void;
+    afterClose: () => void;
     children?: ReactNode;
 }
 
-// TODO Возможно стоит убрать дженерик
-export class AppDialog<T> extends React.Component<AppDialogProps<T>> {
+export class AppDialog extends React.Component<AppDialogProps> {
 
-    // TODO Убрать всплытие ивента
     public handleOverlayClick(): void {
         this.props.afterClose();
     }
@@ -19,7 +17,8 @@ export class AppDialog<T> extends React.Component<AppDialogProps<T>> {
         return this.props.isOpen ? (
             <div className='app-dialog__overlay'
                  onClick={() => this.handleOverlayClick()}>
-                <div className="app-dialog__content">
+                <div className="app-dialog__content"
+                     onClick={event => event.stopPropagation()}>
                     {this.props.children}
                 </div>
             </div>
