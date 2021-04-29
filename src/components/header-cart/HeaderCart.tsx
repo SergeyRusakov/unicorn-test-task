ec6cf3d560f35df19d036f1fcf407b6f22d6a572
Sergey Rusakov <sergeyrusakov1@yandex.ru>
2021-04-29 23:45:02 +0500

    Refactor header cart to functional component

--- src/components/header-cart/HeaderCart.tsx
+++ src/components/header-cart/HeaderCart.tsx
@@ -1,48 +1,34 @@
-import React, { ReactNode } from 'react';
+import React, { useState } from 'react';
 import './HeaderCart.css';
 import { CartStats } from '../cart-stats/CartStats';
 import { CartDialog } from '../cart-dialog/CartDialog';
 
 // TODO Переделать на функциональный компонент с хуками
+export const HeaderCart = () => {
 
-interface HeaderCartState {
-    isCartDialogOpen: boolean;
-}
+    const [isCartDialogOpen, setCardDialogOpen] = useState(false);
 
-export class HeaderCart extends React.Component<any, HeaderCartState> {
-    // TODO Переделать с тоггла на открытие, так как закрываться будет по оверлею
-    public handleTitleClick = () => {
-        this.setState((state) => ({
-            isCartDialogOpen: !state.isCartDialogOpen
-        }));
+    const handleTitleClick = () => {
+        setCardDialogOpen(!isCartDialogOpen);
     }
 
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            isCartDialogOpen: false,
-        };
-    }
+    return (
+        <div className='header-cart'>
 
-    public render(): ReactNode {
-        return (
-            <div className='header-cart'>
-
-                <div className='header-cart__title'
-                     onClick={this.handleTitleClick}>
-                    Корзина
-                </div>
-
-                <div className='header-cart__info'>
-                    <CartStats/>
-                </div>
-
-                <div className='header-cart__cart-dialog'>
-                    {this.state.isCartDialogOpen && <CartDialog/>}
-                </div>
-
+            <div className='header-cart__title'
+                 onClick={handleTitleClick}>
+                Корзина
             </div>
-        );
-    }
+
+            <div className='header-cart__info'>
+                <CartStats/>
+            </div>
+
+            <div className='header-cart__cart-dialog'>
+                {isCartDialogOpen && <CartDialog/>}
+            </div>
+
+        </div>
+    );
 
 }
